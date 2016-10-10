223554872e94a84bf7c6cf92a6ad01219d0720ff
Bill Smith <snowmanam2@gmail.com>
2016-10-09 20:18:20 -0500

    Event registration, messages, other fixes

--- src/main/java/com/gmail/snowmanam2/factionsflight/EngineFlight.java
+++ src/main/java/com/gmail/snowmanam2/factionsflight/EngineFlight.java
@@ -9,24 +9,31 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
+import com.massivecraft.factions.Rel;
 import com.massivecraft.factions.entity.BoardColl;
 import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.MPerm;
 import com.massivecraft.factions.entity.MPlayer;
 import com.massivecraft.factions.event.EventFactionsChunksChange;
 import com.massivecraft.massivecore.ps.PS;
+import com.massivecraft.massivecore.util.MUtil;
 
-public class EngineFlight {
+public class EngineFlight implements Listener {
 	public static EngineFlight i = new EngineFlight();
 	public static Set<Player> fallingPlayers = new HashSet<Player>();
 	public static EngineFlight get() {
 		return i;
+	}
+	
+	public static void registerPermissions() {
+		MPerm.getCreative(25000, "fly", "fly", "fly", MUtil.set(Rel.LEADER, Rel.OFFICER, Rel.MEMBER, Rel.RECRUIT, Rel.ALLY), false, true, true);
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
@@ -42,8 +49,8 @@ public class EngineFlight {
 		Location to = event.getTo();
 		Player player = event.getPlayer();
 		
-		if (((Entity) player).isOnGround() && EngineFlight.fallingPlayers.contains(player)) {
-			EngineFlight.fallingPlayers.remove(player);
+		if (((Entity) player).isOnGround() && fallingPlayers.contains(player)) {
+			fallingPlayers.remove(player);
 		}
 		
 		boolean moved = false;
@@ -66,15 +73,15 @@ public class EngineFlight {
 		updatePlayerFlightStatus(event.getPlayer());
 	}
 	
-	@EventHandler
-	public void onEntityDamage(EntityDamageEvent event) {
-		Entity entity = event.getEntity();
-		if (entity instanceof Player && event.getCause() == DamageCause.FALL) {
-			Player player = (Player) entity;
-			if (EngineFlight.fallingPlayers.contains(player)) {
-				event.setCancelled(true);
-			} else if (player.hasPermission("factionsflight.nodamage")) {
-				event.setCancelled(true);
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onEntityDamageEvent(EntityDamageEvent e) {
+		if (!(e.getEntity() instanceof Player)) {
+			return;
+		}
+		Player p = (Player) e.getEntity();
+		if (e.getCause() == DamageCause.FALL) {
+			if (fallingPlayers.contains(p)) {
+				e.setCancelled(true);
 			}
 		}
 	}
@@ -84,18 +91,29 @@ public class EngineFlight {
 	}
 	
 	public void updatePlayerFlightStatus (Player player, Location location) {
-		PS psChunk = PS.valueOf(location.getWorld().getName(), location.getBlockX()>>4, location.getBlockX()>>4);
+		if (player.hasPermission("factionsflight.bypass")) {
+			return;
+		}
+		
+		PS psChunk = PS.valueOf(location.getWorld().getName(), location.getBlockX()>>4, location.getBlockZ()>>4);
 		Faction faction = BoardColl.get().getFactionAt(psChunk);
 		MPlayer mplayer = MPlayer.get(player);
 		
 		MPerm flyPerm = MPerm.get("fly");
 		if (flyPerm.has(mplayer, faction, false)) {
+			if (!player.getAllowFlight()) {
+				player.sendMessage(Messages.get("flightEnabled"));
+			}
 			player.setAllowFlight(true);
+			
 		} else {
-			if (player.getAllowFlight() && !player.hasPermission("factionsflight.bypass")) {
+			if (player.getAllowFlight()) {
 				player.setAllowFlight(false);
-				EngineFlight.fallingPlayers.add(player);
 				player.sendMessage(flyPerm.createDeniedMessage(mplayer, faction));
+
+				if (!((Entity) player).isOnGround()) {
+					fallingPlayers.add(player);
+				}
 			}
 		}
 	}
--- src/main/java/com/gmail/snowmanam2/factionsflight/FactionsFlight.java
+++ src/main/java/com/gmail/snowmanam2/factionsflight/FactionsFlight.java
@@ -1,7 +1,13 @@
 package com.gmail.snowmanam2.factionsflight;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FactionsFlight extends JavaPlugin {
-
+	@Override
+	public void onEnable() {
+		Messages.loadMessages(this);
+		Bukkit.getPluginManager().registerEvents(EngineFlight.get(), this);
+		EngineFlight.registerPermissions();
+	}
 }
