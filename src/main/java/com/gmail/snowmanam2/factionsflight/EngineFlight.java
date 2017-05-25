a45c67de5b5447df7102e8268a5f9ea7a17c1dd4
Bill Smith <snowmanam2@gmail.com>
2017-05-24 22:25:47 -0500

    Convert to time-based check of flight status

--- src/main/java/com/gmail/snowmanam2/factionsflight/EngineFlight.java
+++ src/main/java/com/gmail/snowmanam2/factionsflight/EngineFlight.java
@@ -1,9 +1,11 @@
 package com.gmail.snowmanam2.factionsflight;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Entity;
@@ -13,26 +15,21 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import com.massivecraft.factions.Rel;
 import com.massivecraft.factions.entity.BoardColl;
 import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.MPerm;
 import com.massivecraft.factions.entity.MPlayer;
-import com.massivecraft.factions.event.EventFactionsChunksChange;
-import com.massivecraft.factions.event.EventFactionsDisband;
-import com.massivecraft.factions.event.EventFactionsRankChange;
-import com.massivecraft.factions.event.EventFactionsRelationChange;
 import com.massivecraft.massivecore.ps.PS;
 import com.massivecraft.massivecore.util.MUtil;
 
 public class EngineFlight implements Listener {
 	public static EngineFlight i = new EngineFlight();
 	public static Set<OfflinePlayer> fallingPlayers = new HashSet<OfflinePlayer>();
+	private static int radius;
 	public static EngineFlight get() {
 		return i;
 	}
@@ -40,60 +37,19 @@ public class EngineFlight implements Listener {
 	public static void init (JavaPlugin plugin) {
 		Bukkit.getPluginManager().registerEvents(EngineFlight.get(), plugin);
 		registerPermissions();
+		
+		EngineFlight.radius = plugin.getConfig().getInt("disableRadius");
+		final long taskTickInterval = plugin.getConfig().getInt("taskTickInterval");
+		BukkitScheduler scheduler = plugin.getServer().getScheduler();
+		scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
+			public void run() {
+				updatePlayerFlight();
+			}
+		}, 0L, taskTickInterval);
 	}
 	
 	public static void registerPermissions() {
 		MPerm.getCreative(25000, "fly", "fly", "fly", MUtil.set(Rel.LEADER, Rel.OFFICER, Rel.MEMBER, Rel.RECRUIT, Rel.ALLY), false, true, true);
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-	public void factionsChunksChange(EventFactionsChunksChange e) {
-		updateAllPlayersFlightStatus();
-	}
-	
-	@EventHandler
-	public void factionsDisband(EventFactionsDisband e) {
-		updateAllPlayersFlightStatus();
-	}
-	
-	@EventHandler
-	public void factionsRelationChange(EventFactionsRelationChange e) {
-		updateAllPlayersFlightStatus();
-	}
-	
-	@EventHandler
-	public void factionsRankChange(EventFactionsRankChange e) {
-		updatePlayerFlightStatus(e.getMPlayer().getPlayer());
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-	public void onPlayerMoveEvent(PlayerMoveEvent event) {
-		Location from = event.getFrom();
-		Location to = event.getTo();
-		Player player = event.getPlayer();
-		
-		if (((Entity) player).isOnGround() && fallingPlayers.contains(player)) {
-			fallingPlayers.remove(player);
-		}
-		
-		boolean moved = false;
-		moved = moved || from.getWorld() != to.getWorld();
-		moved = moved || Math.abs(from.getBlockX()>>4 - to.getBlockX()>>4) > 0;
-		moved = moved || Math.abs(from.getBlockZ()>>4 - to.getBlockZ()>>4) > 0;
-		
-		if (moved) {
-			updatePlayerFlightStatus(player, to);
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onPlayerTeleport(PlayerTeleportEvent event) {
-		updatePlayerFlightStatus(event.getPlayer(), event.getTo());
-	}
-	
-	@EventHandler
-	public void onPlayerJoinEvent(PlayerJoinEvent event) {
-		updatePlayerFlightStatus(event.getPlayer());
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
@@ -111,20 +67,79 @@ public class EngineFlight implements Listener {
 		}
 	}
 	
-	public void updateAllPlayersFlightStatus () {
-		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-			updatePlayerFlightStatus(player);
+	public static void updatePlayerFlight () {
+		Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
+		for (Player player : players) {
+			
+			if (player.hasPermission("factionsflight.bypass") || player.getGameMode().equals(GameMode.CREATIVE)) {
+				player.setAllowFlight(true);
+				return;
+			}
+
+			/* Clean up falling players */
+			if (((Entity) player).isOnGround() && fallingPlayers.contains(player)) {
+				fallingPlayers.remove(player);
+			}
+			
+			/* Territory handling */
+			boolean flightAllowedTerritory = getPlayerFlightTerritory(player);
+			
+			/* Player proximity handling */
+			boolean flightAllowedProximity = getPlayerFlightProximity(player);
+			
+			/* Only take action when there are changes in flight status */
+			if (!player.getAllowFlight() && flightAllowedTerritory && flightAllowedProximity) {
+
+				enableFlight(player);
+			}
+			
+			if (player.getAllowFlight() && !flightAllowedTerritory) {
+				Faction faction = BoardColl.get().getFactionAt(PS.valueOf(player.getLocation()));
+				MPlayer mplayer = MPlayer.get(player);
+				MPerm flyPerm = MPerm.get("fly");
+				
+				disableFlight(player, flyPerm.createDeniedMessage(mplayer, faction));
+				
+			} else if (player.getAllowFlight() && !flightAllowedProximity) {
+				
+				disableFlight(player, Messages.get("flightDisabledProximity"));
+			}
 		}
 	}
 	
-	public void updatePlayerFlightStatus (Player player) {
-		updatePlayerFlightStatus(player, player.getLocation());
+	public static boolean getPlayerFlightProximity (Player player1) {
+		Collection<? extends Player> players = Bukkit.getServer().getOnlinePlayers();
+		
+		boolean flightAllowed = true;
+		
+		for (Player player2 : players) {
+			if (player1 == player2) continue;
+			
+			Location loc1 = player1.getLocation();
+			Location loc2 = player2.getLocation();
+			
+			if (loc1.getWorld() != loc2.getWorld()) continue;
+			
+			double dX = loc1.getX()-loc2.getX();
+			double dZ = loc1.getZ()-loc2.getZ();
+			
+			if (radius*radius > dX*dX + dZ*dZ) {
+				MPlayer mp1 = MPlayer.get(player1);
+				MPlayer mp2 = MPlayer.get(player2);
+				
+				Rel relation = mp1.getRelationTo(mp2);
+				
+				if (!relation.isFriend()) {
+					flightAllowed = false;
+				}
+			}
+		}
+		
+		return flightAllowed;
 	}
 	
-	public void updatePlayerFlightStatus (Player player, Location location) {
-		if (player.hasPermission("factionsflight.bypass")) {
-			return;
-		}
+	public static boolean getPlayerFlightTerritory (Player player) {
+		Location location = player.getLocation();
 		
 		PS psChunk = PS.valueOf(location);
 		Faction faction = BoardColl.get().getFactionAt(psChunk);
@@ -132,20 +147,29 @@ public class EngineFlight implements Listener {
 		
 		MPerm flyPerm = MPerm.get("fly");
 		if (flyPerm.has(mplayer, faction, false)) {
-			if (!player.getAllowFlight()) {
-				player.sendMessage(Messages.get("flightEnabled"));
-			}
-			player.setAllowFlight(true);
+			
+			return true;
 			
 		} else {
-			if (player.getAllowFlight()) {
-				player.setAllowFlight(false);
-				player.sendMessage(flyPerm.createDeniedMessage(mplayer, faction));
-
-				if (!((Entity) player).isOnGround()) {
-					fallingPlayers.add(player);
-				}
-			}
+			
+			return false;
+		}
+	}
+	
+	public static void enableFlight (Player p) {
+		p.sendMessage(Messages.get("flightEnabled"));
+		p.setAllowFlight(true);
+	}
+	
+	public static void disableFlight (Player p, String message) {
+		if (p.isFlying()) {
+			p.sendMessage(message);
+		}
+		
+		p.setAllowFlight(false);
+		
+		if (!((Entity) p).isOnGround()) {
+			fallingPlayers.add(p);
 		}
 	}
 }
--- src/main/java/com/gmail/snowmanam2/factionsflight/FactionsFlight.java
+++ src/main/java/com/gmail/snowmanam2/factionsflight/FactionsFlight.java
@@ -5,6 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FactionsFlight extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		getConfig().addDefault("taskTickInterval", 10);
+		getConfig().addDefault("disableRadius", 32);
+		
 		Messages.loadMessages(this);
 		EngineFlight.init(this);
 	}
